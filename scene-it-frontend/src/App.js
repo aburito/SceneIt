5e232e0d9d36d385701239d14022e8ec7a09c204
Francisco Paliouras <fpalio@franciscos-mbp.lan>
2021-03-12 18:49:33 -0500

    no change

--- scene-it-frontend/src/App.js
+++ scene-it-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-//comment
+
 const App = (props) => {
   return (
     <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
